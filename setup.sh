ca6c0106317f5036e5a274f0adb7d5182f386fe1
Islam98 <47064428+Islam98@users.noreply.github.com>
2024-12-21 15:46:18 +0200

    Add files via upload

---
+++ setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+# Update package lists
+sudo apt update
+
+# Install Docker
+sudo sudo yum install -y docker
+sudo systemctl start docker
+sudo systemctl enable docker
+
+# Install Node.js
+# curl -fsSL https://deb.nodesource.com/setup_18.x | sudo -E bash -
+sudo apt install -y nodejs
+
+# Install PNPM
+npm install -g pnpm
+
+# Initialize the app
+npm init saas-boilerplate myapp3
